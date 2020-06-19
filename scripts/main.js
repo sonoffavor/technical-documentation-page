96ddd212a77b586441cb35d2397caba966382c84
Benjamin Rietman <benjaminrietman@Benjamins-MacBook-Pro.local>
2020-06-18 20:08:51 -0400

    fixed - drop down menu on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,26 @@
 /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
-function myFunction() {
+function openFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+// Function is used for touch devices
+// Close the dropdown if the user clicks outside of it
+window.ontouchstart = function(event) {
+    if (!event.target.matches('.dropbtn') && !event.target.matches('.nav-link')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
